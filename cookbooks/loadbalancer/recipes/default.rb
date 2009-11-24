f946eafaea67592ec8074c5764af1d33a4242d45
Alex Clemesha <clemesha@ucsd.edu>
2009-11-24 15:50:09 -0800

    Recipe and Template improvements for the 'load balanced wep app' scenario.

--- cookbooks/loadbalancer/recipes/default.rb
+++ cookbooks/loadbalancer/recipes/default.rb
@@ -1,1 +1,16 @@
-include_recipe "user"
+package "nginx" do
+  action :install
+end
+
+template "/etc/nginx/nginx.conf" do
+  source "nginx.conf.erb"
+  variables({
+    :public_hostname => @node[:ec2][:public_hostname],
+    :webapp_port => @node[:webapp_port],
+    :webapps => @node[:webapp_ips]
+  })
+end
+
+service "nginx" do
+  action :restart
+end
--- cookbooks/webapp/recipes/default.rb
+++ cookbooks/webapp/recipes/default.rb
@@ -1,3 +1,9 @@
-include_recipe "user"
 include_recipe "twisted"
+
+bash "run-webapp" do
+  user "root"
+  code <<-EOH
+  twistd web --path #{node[:serve_path]} --port 80
+  EOH
+end
 
