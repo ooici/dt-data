0a58523869c11900aa0a4e9f80ccc7d8d6b4297e
Alex Clemesha <clemesha@ucsd.edu>
2009-11-24 20:13:09 -0800

    Create and index page in the webapp example to show the app IP.

--- cookbooks/webapp/recipes/default.rb
+++ cookbooks/webapp/recipes/default.rb
@@ -1,9 +1,15 @@
 include_recipe "twisted"
 
+bash "index-page" do
+  user "root"
+  code <<-EOH
+  echo "This node's IP is '#{node[:ec2][:public_ipv4]}'" > #{node[:serve_path]}/index.html
+  EOH
+end
+
 bash "run-webapp" do
   user "root"
   code <<-EOH
   twistd web --path #{node[:serve_path]} --port 80
   EOH
 end
-
