cad9e8edd3ba0209f4c53a944a3890a7aa2e2993
David LaBissoniere <labisso@uchicago.edu>
2010-10-22 16:04:00 -0500

    Cookbook for copying rabbitmq cookie to homedir

---
+++ cookbooks/rabbitmq_cookie/recipes/default.rb
@@ -0,0 +1,8 @@
+#Cookbook Name: rabbitmq_cookie
+
+bash "copy-rabbitmq-erlang-cookie" do
+  code <<-EOH
+  cp /var/lib/rabbitmq/.erlang.cookie /home/#{node[:username]}/
+  chown #{node[:username]}:#{node[:username]} /home/#{node[:username]}/.erlang.cookie
+  EOH
+end
