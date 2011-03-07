ec4eddd32c99c9b708a26ad7054b0f803a21840c
Jamie Chen <jachen@ucsd.edu>
2011-03-07 09:17:44 -0800

    Added group to user recipe

--- cookbooks/user/recipes/default.rb
+++ cookbooks/user/recipes/default.rb
@@ -2,6 +2,8 @@
 
 user node[:username] do
     comment "Dynamically created user."
+    uid "1000"
+    gid "users"
     home "/home/#{node[:username]}"
     shell "/bin/bash"
     supports :manage_home => true
