87f6ba1de432901fa0dda43a9a6c2bafd49feabf
Tim Freeman <tim@timfreeman.org>
2011-03-10 16:48:24 -0600

    Working for retrieve_method 'git' and 'archive', install_method 'py_venv_setup', and run_method 'sh'

--- cookbooks/burned_ubuntu64_separated/recipes/default.rb
+++ cookbooks/burned_ubuntu64_separated/recipes/default.rb
@@ -1,11 +1,17 @@
 app_archive = "/tmp/app-archive.tar.gz"
 app_dir = "/home/#{node[:username]}/app"
+venv_dir = "/home/#{node[:username]}/app-venv"
 
-bash "Cleanup app_dir" do
-  code <<-EOH
-  rm -rf #{app_dir}
-  EOH
+execute "Cleanup app dir" do
+  command "rm -rf #{app_dir}"
 end
+execute "Cleanup virtualenv dir" do
+  command "rm -rf #{venv_dir}"
+end
+
+########################################################################
+# RETRIEVAL
+########################################################################
 
 case node[:appretrieve][:retrieve_method]
 when "archive"
@@ -19,29 +25,75 @@ when "archive"
     owner node[:username]
     group node[:username]
   end
-  bash "expand-archive" do
-    code <<-EOH
-    cd /tmp
-    tar xzf #{app_archive}
-    mv /tmp/app-archive #{app_dir}
-    mv #{app_archive} /tmp/previous__app-archive.tar.gz
-    EOH
+  directory "/tmp/expand_tmp" do
+    owner "root"
+    group "root"
+    mode "0755"
+    action :create
   end
-end
+  execute "untar app" do
+    cwd "/tmp/expand_tmp"
+    command "tar xzf #{app_archive}"
+  end
+  execute "move app" do
+    command "mv /tmp/expand_tmp/* #{app_dir}"
+  end
+  execute "archive the tarball" do
+    command "mv #{app_archive} /tmp/previous__app-archive.tar.gz"
+  end
 when "git"
-  bash "get-git" do
+  directory app_dir do
+    owner node[:username]
+    group node[:username]
+    mode "0755"
+    action :create
+  end
+  execute "clone the repository" do
+    command "git clone #{node[:appretrieve][:git_repo]} #{app_dir}/"
+  end
+  execute "fetch all code" do
+    cwd app_dir
+    command "git fetch --all"
+  end
+  execute "checkout the desired branch" do
+    cwd app_dir
+    command "git checkout -b activebranch origin/#{node[:appretrieve][:git_branch]}"
+  end
+  # This makes HEAD meaningful: 
+  execute "move branch to latest" do
+    cwd app_dir
+    command "git pull"
+  end
+  execute "move branch to commit or reference" do
+    cwd app_dir
+    command "git reset --hard #{node[:appretrieve][:git_commit]}"
+  end
+else raise ArgumentError, "unknown retrieve_method #{node[:appretrieve][:retrieve_method]}, should be 'archive' or 'git'"
+end
+
+########################################################################
+# INSTALLATION
+########################################################################
+
+case node[:appinstall][:install_method]
+when "py_venv_setup"
+  package "python-virtualenv" do
+    action :install
+  end
+  execute "create virtualenv" do
+    command "virtualenv --no-site-packages #{venv_dir}"
+  end
+  bash "run install" do
+    cwd app_dir
     code <<-EOH
-    mkdir -p #{app_dir}
-    git clone #{node[:appretrieve][:git_repo]} #{app_dir}/
-    cd #{app_dir}
-    git fetch --all
-    git checkout -b activebranch origin/#{node[:appretrieve][:git_branch]}
-    git pull
-    git reset --hard #{node[:appretrieve][:git_commit]}
+    source #{venv_dir}/bin/activate
+    #{venv_dir}/bin/python setup.py install
     EOH
   end
+when "py_venv_buildout"
+  raise ArgumentError, "not support 'py_venv_buildout' install_method just yet"
+else raise ArgumentError, "unknown install_method #{node[:appinstall][:install_method]}"
 end
-else raise ArgumentError, 'retrieve_method is not "archive" or "git"'
 
 bash "give-app-user-ownership" do
   code <<-EOH
@@ -51,6 +103,10 @@ bash "give-app-user-ownership" do
   fi
   EOH
 end
+
+########################################################################
+# ACCESS
+########################################################################
 
 bash "give-remote-user-log-access" do
   code <<-EOH
@@ -62,5 +118,87 @@ bash "give-remote-user-log-access" do
   fi
   chown -R #{node[:username]} /home/#{node[:username]}/.ssh
   EOH
+end
+
+########################################################################
+# RUN
+########################################################################
+   
+case node[:apprun][:run_method]
+when "sh"
+  
+  ######################################################################
+  # RUN SH
+  ######################################################################
+    
+  template "#{app_dir}/res/logging/loglevels.cfg" do
+    source "loglevels.cfg.erb"
+    owner "#{node[:username]}"
+    variables(:log_level => node[:pythoncc][:log_level])
+  end
+  
+  node[:services].each do |service, service_spec|
+  
+    service_config = "#{app_dir}/res/config/#{service}-ionservices.cfg"
+  
+    template "#{service_config}" do
+      source "ionservices.cfg.erb"
+      owner "#{node[:username]}"
+      variables(:service_spec => service_spec)
+    end
+    
+    logging_dir = "#{app_dir}/logs/#{service}"
+    directory "#{logging_dir}" do
+      owner "#{node[:username]}"
+      group "#{node[:username]}"
+      mode "0755"
+    end
+    
+    logging_config = "#{logging_dir}/#{service}-logging.conf"
+        
+    template "#{logging_config}" do
+      source "ionlogging.conf.erb"
+      owner "#{node[:username]}"
+      variables(:service_name => service)
+    end
+  
+    bash "service-script" do
+      user node[:username]
+      cwd app_dir
+      code <<-EOH
+      echo "#!/bin/bash" >> start-#{service}.sh
+      if [ -f /opt/cei_environment ]; then
+        source /opt/cei_environment
+        echo "source /opt/cei_environment" >> start-#{service}.sh
+      fi
+      if [ -f #{venv_dir}/bin/activate ]; then
+        echo "source #{venv_dir}/bin/activate" >> start-#{service}.sh
+      fi
+      echo "export ION_ALTERNATE_LOGGING_CONF=#{logging_config}" >> start-#{service}.sh
+      echo "twistd --pidfile=#{service}-service.pid cc -n -h #{node[:pythoncc][:broker]} --broker_heartbeat=#{node[:pythoncc][:broker_heartbeat]} -a processes=#{service_config},sysname=#{node[:pythoncc][:sysname]} res/scripts/newcc.py" >> start-#{service}.sh
+      chmod +x start-#{service}.sh
+      EOH
+    end
+  
+    execute "start-service" do
+      not_if { node.include? :do_not_start and node[:do_not_start].include? service }
+      user node[:username]
+      cwd app_dir
+      environment({
+        "HOME" => "/home/#{node[:username]}",
+        "ION_ALTERNATE_LOGGING_CONF" => "#{logging_config}"
+      })
+      command "./start-#{service}.sh"
+    end
+  end
+
+when "supervised"
+  
+  ######################################################################
+  # RUN SUPERVISED
+  ######################################################################
+  raise ArgumentError, "not support 'supervised' run_method just yet"
+
+else raise ArgumentError, "unknown install_method #{node[:apprun][:run_method]}"
 end
 
