1d97a8b5b47a7dce174167d3dd7ce0e2595a50d2
Adam R. Smith <a2smith@ucsd.edu>
2011-03-14 15:45:40 -0700

    upgrade rabbit to the latest package

--- cookbooks/rabbitmq/recipes/default.rb
+++ cookbooks/rabbitmq/recipes/default.rb
@@ -17,6 +17,15 @@
 # limitations under the License.
 #
 
+bash "rabbitmq-repo" do
+  code <<-EOH
+  echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
+  wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+  sudo apt-key add rabbitmq-signing-key-public.asc
+  apt-get update
+  EOH
+end
+
 package "rabbitmq-server" do
   action :install
 end
