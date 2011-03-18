0c8cc847f4e3cbc884f9ec0e5a03c79f6364f4fa
Tim Freeman <tim@timfreeman.org>
2011-03-18 06:51:55 -0500

    Do not install context agent if it exists

--- cookbooks/nimbus_context_agent/recipes/default.rb
+++ cookbooks/nimbus_context_agent/recipes/default.rb
@@ -1,17 +1,12 @@
 #Cookbook Name: nimbus_context_agent
 
-directory "/opt/nimbus" do
-  mode "0755"
-  action :create
-  not_if "test -d /opt/nimbus"
-end
-
 bash "install-nimbus-context-agent" do
   code <<-EOH
-    wget http://www.nimbusproject.org/downloads/nimbus-ctx-agent-2.2.1.tar.gz
-    tar zxf nimbus-ctx-agent-2.2.1.tar.gz
-    mv nimbus-ctx-agent-2.2.1/* /opt/nimbus/
-    chmod -R +x /opt/nimbus/ctx-scripts/*
+    if [ ! -d /opt/nimbus ]; then
+      wget http://www.nimbusproject.org/downloads/nimbus-ctx-agent-2.3.0.tar.gz
+      tar zxf nimbus-ctx-agent-2.3.0.tar.gz
+      mv nimbus-ctx-agent-2.3.0/ /opt/nimbus/
+    fi
   EOH
 end
 
