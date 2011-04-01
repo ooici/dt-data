403dab94f4706eb04481055640a6459b3163ae42
David LaBissoniere <labisso@uchicago.edu>
2011-04-01 15:57:29 -0500

    RabbitMQ vhost creation and permissions

--- cookbooks/rabbitmq/recipes/default.rb
+++ cookbooks/rabbitmq/recipes/default.rb
@@ -15,6 +15,15 @@ template "/etc/rabbitmq/rabbitmq.conf" do
   notifies :restart, resources(:service => "rabbitmq-server"), :immediately
 end
 
+if node[:rabbitmq].include? :vhosts
+  node[:rabbitmq][:vhosts].each do |vhost|
+    execute "Add vhost #{vhost}" do
+      return [0,2] # ok if vhost already exists
+      command "rabbitmqctl -q add_vhost #{vhost}"
+    end
+  end
+end
+
 if node[:rabbitmq].include? :users and not node[:rabbitmq][:users].empty?
 
   execute "Remove default guest user" do
@@ -22,9 +31,18 @@ if node[:rabbitmq].include? :users and not node[:rabbitmq][:users].empty?
     command "rabbitmqctl -q delete_user guest"
   end
 
-  node[:rabbitmq][:users].each do |username, password|
+  node[:rabbitmq][:users].each do |username, spec|
     execute "Add user #{username}" do
-      command "rabbitmqctl -q add_user #{username} #{password}"
+      command "rabbitmqctl -q add_user #{username} #{spec[:password]}"
+    end
+    
+    if spec.include? :permissions
+      spec[:permissions].each do |vhost, perms|
+        execute "Set permissions for user=#{username} vhost=#{vhost}" do
+          command "rabbitmqctl -q set_permissions -p #{vhost} #{username} "+
+            "'#{perms.conf}' '#{perms.write}' '#{perms.read}'"
+        end
+      end
     end
   end
 end
