21cd214e08cfbaa64b4ba483615be61e5a510f59
Tim Freeman <tim@timfreeman.org>
2011-04-08 16:10:51 -0500

    Previous containerconfig version did not properly escape complicated configurations

--- cookbooks/r1app/definitions/ioncontainer_config.rb
+++ cookbooks/r1app/definitions/ioncontainer_config.rb
@@ -18,42 +18,59 @@ define :ioncontainer_config, :user => nil, :group => nil, :ioncontainer_name => 
     EOH
   end
   
+  # The impedance mismatch between JSON and Python rears its head 
   params[:ioncontainer_spec].each do |app_name, app_config|
     
-    one_app = "{'name':'#{app_config[:name]}', "
-    one_app << "'version':'#{app_config[:version]}', "
-    if app_config.include? :mult and app_config[:mult]
-      one_app << "'mult':True, "
-    end
-    one_app << "'config':{"
-    app_config[:config].each do |modname, kvs|
-      one_app << "'#{modname}':{ "
-      kvs.each do |key, value|
+    ruby_block "build-appconf1" do
+      block do
+        one_app = "\n    {'name':'#{app_config[:name]}', "
+        one_app << "\n     'version':'#{app_config[:version]}', "
+        if app_config.include? :mult and app_config[:mult]
+          one_app << "\n     'mult':True, "
+        end
+        one_app << "\n     'args':{"
+        app_config[:args].each do |key, value|
           one_app << "'#{key}':'#{value}', "
+        end
+        one_app << "},\n     'config':{"
+        File.open(params[:name], 'a') {|f| f.write(one_app) }
       end
-      one_app << "}, "
     end
-    one_app << "}, 'args':{"
-    app_config[:args].each do |key, value|
-      one_app << "'#{key}':'#{value}', "
+    app_config[:config].each do |modname, keyvalue_dict|
+      ruby_block "start-mod" do
+        block do
+          one_mod_start = "\n               '#{modname}':{ "
+          File.open(params[:name], 'a') {|f| f.write(one_mod_start) }
+        end
+      end
+      keyvalue_dict.each do |l_key, l_value|
+        ruby_block "build-mod-kv" do
+          block do
+            kv_line = "     '#{l_key}': '" + l_value.to_s + "',\n"
+            File.open(params[:name], 'a') {|f| f.write(kv_line) }
+          end
+        end
+      end
+      ruby_block "end-mod" do
+        block do
+          one_mod_end = " },\n"
+          File.open(params[:name], 'a') {|f| f.write(one_mod_end) }
+        end
+      end
     end
-    one_app << "}},"
     
-    bash "create app line" do
-      user "#{params[:user]}"
-      group "#{params[:group]}"
-      code <<-EOH
-      echo "#{one_app}" >> #{params[:name]}
-      chmod 600 #{params[:name]}
-      EOH
+    ruby_block "end-app" do
+      block do
+        app_end = "}},\n"
+        File.open(params[:name], 'a') {|f| f.write(app_end) }
+      end
     end
   end
   
-  bash "finish rel file" do
-    user "#{params[:user]}"
-    group "#{params[:group]}"
-    code <<-EOH
-    echo -e "  ]\n}\n" >> #{params[:name]}
-    EOH
+  ruby_block "finish-file" do
+    block do
+      file_end = "  ]\n}\n"
+      File.open(params[:name], 'a') {|f| f.write(file_end) }
+    end
   end
 end
