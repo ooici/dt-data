a9de41d5748dfc19931eda7c246d7d750d18b9aa
David LaBissoniere <labisso@uchicago.edu>
2011-04-18 13:17:23 -0500

    python_datafile-ified ionlocal_config definition

--- cookbooks/r1app/definitions/ionlocal_config.rb
+++ cookbooks/r1app/definitions/ionlocal_config.rb
@@ -3,62 +3,13 @@ define :ionlocal_config, :user => nil, :group => nil, :universals => {},
   
   raise ArgumentError, 'user must be specified' if params[:user].nil?
   raise ArgumentError, 'group must be specified' if params[:group].nil?
-
-  # The following excruciating ionlocal.config work should be a Ruby block
-  bash "create ionlocal.config" do
-    user "#{params[:user]}"
-    group "#{params[:group]}"
-    code <<-EOH
-    echo -e "{\n'epu.universal':{" > #{params[:name]}
-    chmod 600 #{params[:name]}
-    EOH
-  end
-  params[:universals].each do |u_key, u_value|
-    bash "add universals to ionlocal.config" do
-      user "#{params[:user]}" 
-      group "#{params[:group]}"
-      code <<-EOH
-      echo "    '#{u_key}': '#{u_value}'," >> #{params[:name]}
-      EOH
-    end
-  end
-  bash "modify ionlocal.config" do
-    user "#{params[:user]}"
-    group "#{params[:group]}"
-    code <<-EOH
-    echo "}," >> #{params[:name]}
-    EOH
-  end
-  # Brutal:
+  cfg = {'epu.universal' => params[:universals].to_hash}
   params[:locals].each do |section_name, keyvalue_dict|
-    bash "add local section to ionlocal.config" do
-      user "#{params[:user]}" 
-      group "#{params[:group]}"
-      code <<-EOH
-      echo "'#{section_name}':{" >> #{params[:name]}
-      EOH
-    end
-    keyvalue_dict.each do |l_key, l_value|
-      ruby_block "build-line" do
-        block do
-          config_line = "     '#{l_key}': '" + l_value.to_s + "',\n"
-          File.open(params[:name], 'a') {|f| f.write(config_line) }
-        end
-      end
-    end
-    bash "finish local section to ionlocal.config" do
-      user "#{params[:user]}" 
-      group "#{params[:group]}"
-      code <<-EOH
-      echo "}," >> #{params[:name]}
-      EOH
-    end
+    cfg[section_name] = keyvalue_dict.to_hash
   end
-  bash "finish ionlocal.config" do
-    user "#{params[:user]}" 
-    group "#{params[:group]}"
-    code <<-EOH
-    echo "}" >> #{params[:name]}
-    EOH
+
+  cfgfile = params[:name]
+  python_datafile cfgfile do
+    data cfg
   end
 end
