364293f283caade4b1dee0afc3a624666129c711
David LaBissoniere <labisso@uchicago.edu>
2011-04-18 13:02:14 -0500

    Set permissions of rel file

--- cookbooks/r1app/definitions/python.rb
+++ cookbooks/r1app/definitions/python.rb
@@ -10,9 +10,17 @@ define :python_datafile, :data => nil do
       t.close()
     end
   end
+  
+  # in chef 0.9+ this should be cookbook_file
   remote_file "/tmp/json2py.py" do
     source "json2py.py"
     mode "0755"
+  end
+
+  file params[:name] do
+    owner node[:username]
+    group node[:groupname]
+    mode "0600"
   end
   execute "/tmp/json2py.py #{t.path} #{params[:name]}"
 end
