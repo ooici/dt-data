2c9feab7ed9823229953224e45737adc4d4aea05
David LaBissoniere <labisso@uchicago.edu>
2011-05-02 11:03:54 -0500

    Trust another gpg key for cassandra packages

--- cookbooks/cassandra/recipes/default.rb
+++ cookbooks/cassandra/recipes/default.rb
@@ -25,6 +25,8 @@ bash "add-cassandra-repo" do
   echo "deb-src http://www.apache.org/dist/cassandra/debian 07x main" >> /etc/apt/sources.list.d/apache.list
   gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
   gpg --export --armor F758CE318D77295D | sudo apt-key add -
+  gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00
+  gpg --export --armor 2B5C1B00 | sudo apt-key add -
   apt-get update
   EOH
 end
