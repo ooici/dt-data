a063e5f3721579eed5d6df2a8c22ef57080ec183
Patrick Armstrong <patricka@uvic.ca>
2012-02-16 17:20:31 -0800

    Add registering with loggly to rsyslog recipe

--- cookbooks/rsyslog/recipes/default.rb
+++ cookbooks/rsyslog/recipes/default.rb
@@ -9,10 +9,19 @@ directory node[:rsyslog][:directory] do
   action :create
 end
 
-template "/etc/rsyslog.d/#{node[:rsyslog][:config_priority]}-epu.conf" do
-  source "epu.conf.erb"
+template "/etc/rsyslog.d/#{node[:rsyslog][:config_priority]}-#{node[:rsyslog][:name]}.conf" do
+  source "rsyslog.conf.erb"
   owner node[:rsyslog][:user]
   group node[:rsyslog][:user]
+end
+
+bash "register with loggly" do
+    code <<-EOH
+    curl -X POST -u #{node[:rsyslog][:loggly][:username]} http://#{node[:rsyslog][:loggly][:subdomain]}.loggly.com/api/inputs/#{node[:rsyslog][:loggly][:port]}/adddevice
+    EOH
+    only_if {   node[:rsyslog][:loggly][:port] 
+             && node[:rsyslog][:loggly][:subdomain]
+             && node[:rsyslog][:loggly][:username]}
 end
 
 service "rsyslog" do
