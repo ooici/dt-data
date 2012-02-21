ccf30311784d528c6ee52527a4b4adc29c3f093c
Patrick Armstrong <patricka@uvic.ca>
2012-02-21 15:38:26 -0800

    Allow configuring ports and servers in zookeeper

--- cookbooks/zookeeper/attributes/zookeper.rb
+++ cookbooks/zookeeper/attributes/zookeper.rb
@@ -3,3 +3,6 @@ default[:zookeeper][:initLimit] = 10
 default[:zookeeper][:syncLimit] = 5
 default[:zookeeper][:dataDir] = "/var/lib/zookeeper"
 default[:zookeeper][:clientPort] = 2181
+default[:zookeeper][:servers] = ["localhost"]
+default[:zookeeper][:leaderPort] = 2888
+default[:zookeeper][:electionPort] = 3888
