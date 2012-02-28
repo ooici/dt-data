7fd0be24e07976b6d493b18269aae00796693916
BuzzTroll <buzztroll@gmail.com>
2012-02-28 10:20:52 -1000

    adding in the env config

--- cookbooks/rabbitmq/recipes/default.rb
+++ cookbooks/rabbitmq/recipes/default.rb
@@ -7,6 +7,15 @@ service "rabbitmq-server" do
   action [ :enable, :start ]
 end
 
+template "/etc/rabbitmq/rabbitmq-env.conf" do
+  source "rabbitmq-env.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, resources(:service => "rabbitmq-server"), :immediately
+end
+
+
 template "/etc/rabbitmq/rabbitmq.config" do
   source "rabbitmq.config.erb"
   owner "root"
