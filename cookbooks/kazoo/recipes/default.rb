f13f0a0cc65c23071d5af3a26f4fd8073a8b3ce4
David LaBissoniere <labisso@gmail.com>
2012-03-02 15:40:24 -0600

    Add cookbook for kazoo

---
+++ cookbooks/kazoo/recipes/default.rb
@@ -0,0 +1,12 @@
+case node[:platform]
+  when "debian","ubuntu"
+    package "python-zookeeper" do
+      action :install
+    end
+end
+
+execute "install kazoo" do
+  user node[:username]
+  group node[:groupname]
+  command "pip install -e 'git+git://github.com/nimbusproject/kazoo.git#egg=kazoo'"
+end
