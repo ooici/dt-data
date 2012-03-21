519b0b52f63ed24a3ca16237ab61a7a23b8aab61
BuzzTroll <buzztroll@gmail.com>
2012-03-21 09:20:17 -1000

    cleaning up cruft from the example chef template.

--- cookbooks/rabbitmqcerts/metadata.rb
+++ cookbooks/rabbitmqcerts/metadata.rb
@@ -1,4 +1,4 @@
-license          "All rights reserved"
+license          "Apache2"
 description      "Creates A CA and Signed SSL Cert."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0"
