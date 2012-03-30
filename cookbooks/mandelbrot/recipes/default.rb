f196893570461c75aed61a8c8f251d09eef82b67
Pierre Riteau <priteau@ci.uchicago.edu>
2012-03-31 01:27:24 +0200

    Fix

--- cookbooks/mandelbrot/recipes/default.rb
+++ cookbooks/mandelbrot/recipes/default.rb
@@ -1,5 +1,5 @@
 execute "Configure meta file" do
-  command "cat > /usr/local/src/ExperimentBuilder <<EOF
+  command "cat > /usr/local/src/ExperimentBuilder/meta <<EOF
 amqp://#{node[:mandelbrot][:rabbitmq_username]}:#{node[:mandelbrot][:rabbitmq_password]}@#{node[:mandelbrot][:rabbitmq_host]}:5672/
 SierraTes2
 EOF"
