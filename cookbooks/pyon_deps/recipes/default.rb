2ac9614aa2392e5ed105822c56806a9609e315a7
Patrick Armstrong <patricka@uvic.ca>
2012-04-20 12:41:45 -0700

    Add netcdf to pyon deps

--- cookbooks/pyon_deps/recipes/default.rb
+++ cookbooks/pyon_deps/recipes/default.rb
@@ -3,7 +3,7 @@ include_recipe "couchdb"
 case node[:platform]
   when "debian","ubuntu"
     # Easy to install packages
-    %w{ libncurses5-dev swig libzmq-dev libevent-dev }.each do |pkg|
+    node[:pyon][:debian_packages].each do |pkg|
       package pkg
     end
 
