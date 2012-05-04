80cfdae7542afb2921c7c8956c4713a21c2ceb48
Patrick Armstrong <patricka@uvic.ca>
2012-05-04 15:25:41 -0700

    Update pyon deps

--- cookbooks/pyon_deps/attributes/default.rb
+++ cookbooks/pyon_deps/attributes/default.rb
@@ -1,1 +1,1 @@
-default[:pyon][:debian_packages] = ["libncurses5-dev" "swig" "libzmq-dev" "libevent-dev" "libhdf5-serial-dev" "python-numpy" "libnetcdf-dev"]
+default[:pyon][:debian_packages] = ["libncurses5-dev", "swig", "libzmq-dev", "libevent-dev", "libhdf5-serial-dev", "python-numpy", "libnetcdf-dev", "libfreetype6-dev", "libpng-dev", "g++"]
