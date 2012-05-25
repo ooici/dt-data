91b030904f2e5de4ec63f6bd361213784d0ee65b
Pierre Riteau <priteau@ci.uchicago.edu>
2012-05-25 15:18:17 +0200

    Use the git Chef resource instead of manual calls

--- cookbooks/r2app/definitions/retrieve.rb
+++ cookbooks/r2app/definitions/retrieve.rb
@@ -1,19 +1,19 @@
 define :retrieve_app, :conf => nil, :user => nil, :group => nil do
-  
-  app_archive = "/tmp/app-archive.tar.gz"
+
   app_dir = params[:name]
   conf = params[:conf]
-  raise ArgumentError, 'app_dir must be specified' if app_dir.nil? or 
+  raise ArgumentError, 'app_dir must be specified' if app_dir.nil? or
     app_dir.empty?
   raise ArgumentError, 'conf must be specified' if conf.nil?
   raise ArgumentError, 'user must be specified' if params[:user].nil?
   raise ArgumentError, 'group must be specified' if params[:group].nil?
 
-  raise ArgumentError, 'retrieve method must be specified' if 
+  raise ArgumentError, 'retrieve method must be specified' if
     not conf.include?(:retrieve_method)
 
   case conf[:retrieve_method]
   when "archive"
+    app_archive = "/tmp/app-archive.tar.gz"
     if not conf[:archive_url] =~ /(.*)\.tar\.gz$/
       raise ArgumentError, 'archive_url is not tar.gz file'
     end
@@ -38,38 +38,19 @@ define :retrieve_app, :conf => nil, :user => nil, :group => nil do
     execute "archive the tarball" do
       command "mv #{app_archive} /tmp/previous__app-archive.tar.gz"
     end
+    execute "give-app-user-ownership" do
+      command "chown -R #{params[:user]}:#{params[:group]} #{app_dir}"
+    end
 
   when "git"
-    execute "clone the repository" do
-      command "git clone #{conf[:git_repo]} #{app_dir}/"
+    git app_dir do
+      repository conf[:git_repo]
+      reference conf[:git_branch]
+      action :sync
+      user params[:user]
+      group params[:group]
     end
-    execute "fetch all code" do
-      cwd app_dir
-      command "git fetch"
-    end
-    execute "checkout the desired branch" do
-      cwd app_dir
-      command "git checkout -b activebranch origin/#{conf[:git_branch]}"
-    end
-    # This makes HEAD meaningful: 
-    execute "move branch to latest" do
-      cwd app_dir
-      command "git pull"
-    end
-    execute "move branch to commit or reference" do
-      cwd app_dir
-      command "git reset --hard #{conf[:git_commit]}"
-    end
-    # This does nothing when a repo has no submodules
-    execute "pull in submodules" do
-      cwd app_dir
-      command "git submodule update --init"
-    end
-  else raise ArgumentError, "unknown retrieve_method #{conf[:retrieve_method]}, should be 'archive' or 'git'"
+  else
+    raise ArgumentError, "unknown retrieve_method #{conf[:retrieve_method]}, should be 'archive' or 'git'"
   end
-  
-  execute "give-app-user-ownership" do
-    command "chown -R #{params[:user]}:#{params[:group]} #{app_dir}"
-  end
-
 end
