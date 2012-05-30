82a8b96bda05c651ebd3a573ad2e7a602e556c47
Pierre Riteau <priteau@ci.uchicago.edu>
2012-05-30 23:09:28 +0200

    Handle epuagent too

--- cookbooks/epu/recipes/default.rb
+++ cookbooks/epu/recipes/default.rb
@@ -7,163 +7,159 @@
 
 require 'tmpdir'
 
-user node[:epu][:username] do
-    comment "Dynamically created user."
-    gid "#{node[:epu][:groupname]}"
-    home "/home/#{node[:epu][:username]}"
-    shell "/bin/bash"
-    supports :manage_home => true
-end
+[ :epu, :epuagent ].each do |app|
 
-include_recipe "python"
-include_recipe "virtualenv"
+  next if node[app].nil?
 
-ve_dir = node[:epu][:virtualenv][:path]
-
-[ :create, :activate ].each do |act|
-  virtualenv ve_dir do
-    owner node[:epu][:username]
-    group node[:epu][:groupname]
-    python node[:epu][:virtualenv][:python]
-    virtualenv node[:epu][:virtualenv][:virtualenv]
-    action act
+  user node[app][:username] do
+      comment "Dynamically created user."
+      gid "#{node[app][:groupname]}"
+      home "/home/#{node[app][:username]}"
+      shell "/bin/bash"
+      supports :manage_home => true
   end
-end
 
-# Other dependencies
-%w{ libevent-dev libncurses5-dev libzmq-dev swig }.each do |pkg|
-  package pkg
-end
+  include_recipe "python"
+  include_recipe "virtualenv"
 
-src_dir = "#{Dir.tmpdir}/epu"
+  ve_dir = node[app][:virtualenv][:path]
 
-if node[:epu][:action].include?("retrieve")
-  case node[:epu][:retrieve_config][:retrieve_method]
-  when "git"
-    include_recipe "git"
-
-    git src_dir do
-      repository node[:epu][:retrieve_config][:git_repo]
-      reference node[:epu][:retrieve_config][:git_reference]
-      action :sync
-      user node[:epu][:username]
-      group node[:epu][:groupname]
+  [ :create, :activate ].each do |act|
+    virtualenv ve_dir do
+      owner node[app][:username]
+      group node[app][:groupname]
+      python node[app][:virtualenv][:python]
+      virtualenv node[app][:virtualenv][:virtualenv]
+      action act
     end
-  else
-    abort "retrieve_method #{node[:epu][:retrieve_config][:retrieve_method]} not implemented yet"
-  end
-end
-
-if node[:epu][:action].include?("install")
-  case node[:epu][:install_config][:install_method]
-  when "py_venv_setup"
-    execute "run install" do
-      cwd src_dir
-      user node[:epu][:username]
-      group node[:epu][:groupname]
-      command "env >/tmp/env ; python setup.py install"
-    end
-    execute "install-supervisor" do
-      user node[:epu][:username]
-      group node[:epu][:groupname]
-      command "easy_install --find-links=#{node[:epu][:install_config][:package_repo]} supervisor"
-    end
-  else
-    abort "install_method #{node[:epu][:install_config][:install_method]} not implemented yet"
-  end
-end
-
-if node[:epu][:action].include?("run")
-  # Create run directory
-  run_dir = node[:epu][:run_config][:run_directory]
-
-  # Create run directory
-  directory run_dir do
-    owner node[:epu][:username]
-    group node[:epu][:groupname]
   end
 
-  # autorestart is for all processes right now, could be made more
-  # granular if needed. Also note that it only applies in "supervised" mode.
-  autorestart = node[:epu][:run_config].fetch("autorestart", false)
-
-  # Name of the service
-  epuservice_name = node[:epu][:run_config][:program]
-
-  # Configuration file for this service
-  epu_config_file = File.join(run_dir, "#{epuservice_name}.yml")
-
-  epu_config epu_config_file do
-    user node[:epu][:username]
-    group node[:epu][:groupname]
-    epuservice_name epuservice_name
-    epuservice_spec node[:epu][:run_config][:config]
+  # Other dependencies
+  %w{ libevent-dev libncurses5-dev libzmq-dev swig }.each do |pkg|
+    package pkg
   end
 
-  base_logging_dir = "#{run_dir}/logs"
-  directory "#{base_logging_dir}" do
-    owner "#{node[:epu][:username]}"
-    group "#{node[:epu][:groupname]}"
-    mode "0755"
-  end
+  src_dir = "#{Dir.tmpdir}/#{app}"
 
-  start_script = File.join(run_dir, "start-#{epuservice_name}.sh")
-  template start_script do
-    source "start-service.sh.erb"
-    owner node[:epu][:username]
-    group node[:epu][:groupname]
-    mode 0755
-    variables(:service => epuservice_name,
-              :service_config => epu_config_file,
-              :venv_dir => ve_dir,
-              :run_dir => run_dir,
-              :background_process => node[:epu][:run_config][:run_method] == "sh"
-             )
-  end
+  if node[app][:action].include?("retrieve")
+    case node[app][:retrieve_config][:retrieve_method]
+    when "git"
+      include_recipe "git"
 
-  # add command to services list
-  epuservice = {
-    :program => epuservice_name,
-    :command => start_script,
-    :autorestart => autorestart
-  }
-
-  case node[:epu][:run_config][:run_method]
-  when "supervised"
-    sup_conf = File.join(run_dir, "supervisor.conf")
-    template sup_conf do
-      source "supervisor.conf.erb"
-      mode 0400
-      owner node[:epu][:username]
-      group node[:epu][:groupname]
-      variables(:epuservice => epuservice)
-    end
-
-    bash "start-supervisor" do
-      user node[:epu][:username]
-      group node[:epu][:groupname]
-      environment({
-        "HOME" => "/home/#{node[:epu][:username]}"
-      })
-      code <<-EOH
-      supervisord -c #{sup_conf}
-      EOH
-    end
-
-=begin
-    # start up the monitor process, if configured
-    if node.include? :appmonitor
-      app_monitor monitor_dir do
-        conf node[:appmonitor]
-        user node[:epu][:username]
-        group node[:epu][:groupname]
-        virtualenv ve_dir
-        supervisor_socket "unix://" + File.join(run_dir, "supervisor.sock")
+      git src_dir do
+        repository node[app][:retrieve_config][:git_repo]
+        reference node[app][:retrieve_config][:git_reference]
+        action :sync
+        user node[app][:username]
+        group node[app][:groupname]
       end
+    else
+      abort "retrieve_method #{node[app][:retrieve_config][:retrieve_method]} not implemented yet"
     end
-=end
+  end
 
-  else
-    abort "run_method #{node[:epu][:run_config][:run_method]} not implemented yet"
+  if node[app][:action].include?("install")
+    case node[app][:install_config][:install_method]
+    when "py_venv_setup"
+      execute "run install" do
+        cwd src_dir
+        user node[app][:username]
+        group node[app][:groupname]
+        command "env >/tmp/env ; python setup.py install"
+      end
+      execute "install-supervisor" do
+        user node[app][:username]
+        group node[app][:groupname]
+        command "easy_install --find-links=#{node[app][:install_config][:package_repo]} supervisor"
+      end
+    else
+      abort "install_method #{node[app][:install_config][:install_method]} not implemented yet"
+    end
+  end
+
+  if node[app][:action].include?("run")
+    # Create run directory
+    run_dir = node[app][:run_config][:run_directory]
+
+    # Create run directory
+    directory run_dir do
+      owner node[app][:username]
+      group node[app][:groupname]
+    end
+
+    # autorestart is for all processes right now, could be made more
+    # granular if needed. Also note that it only applies in "supervised" mode.
+    autorestart = node[app][:run_config].fetch("autorestart", false)
+
+    # Name of the service
+    epuservice_name = node[app][:run_config][:program]
+
+    # Configuration file for this service
+    epu_config_file = File.join(run_dir, "#{epuservice_name}.yml")
+    epu_spec = node[app][:run_config][:config].to_hash
+    if app == :epuagent
+      supervisor_socket = "unix://" + File.join(node[:epu][:run_config][:run_directory], "supervisor.sock")
+      epu_spec["epuagent"].merge!({ "supervisor_socket" => supervisor_socket})
+    end
+
+    epu_config epu_config_file do
+      user node[app][:username]
+      group node[app][:groupname]
+      epuservice_name epuservice_name
+      epuservice_spec epu_spec
+    end
+
+    base_logging_dir = "#{run_dir}/logs"
+    directory "#{base_logging_dir}" do
+      owner "#{node[app][:username]}"
+      group "#{node[app][:groupname]}"
+      mode "0755"
+    end
+
+    start_script = File.join(run_dir, "start-#{epuservice_name}.sh")
+    template start_script do
+      source "start-service.sh.erb"
+      owner node[app][:username]
+      group node[app][:groupname]
+      mode 0755
+      variables(:service => epuservice_name,
+                :service_config => epu_config_file,
+                :venv_dir => ve_dir,
+                :run_dir => run_dir,
+                :background_process => node[app][:run_config][:run_method] == "sh"
+               )
+    end
+
+    epuservice = {
+      :program => epuservice_name,
+      :command => start_script,
+      :autorestart => autorestart
+    }
+
+    case node[app][:run_config][:run_method]
+    when "supervised"
+      sup_conf = File.join(run_dir, "supervisor.conf")
+      template sup_conf do
+        source "supervisor.conf.erb"
+        mode 0400
+        owner node[app][:username]
+        group node[app][:groupname]
+        variables(:epuservice => epuservice)
+      end
+
+      bash "start-supervisor" do
+        user node[app][:username]
+        group node[app][:groupname]
+        environment({
+          "HOME" => "/home/#{node[app][:username]}"
+        })
+        code <<-EOH
+        supervisord -c #{sup_conf}
+        EOH
+      end
+
+    else
+      abort "run_method #{node[app][:run_config][:run_method]} not implemented yet"
+    end
   end
 end
