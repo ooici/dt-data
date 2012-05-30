3aa52f57cf5ffa7f2dbba990d6b7e48d7f208186
Pierre Riteau <priteau@ci.uchicago.edu>
2012-05-30 17:02:39 +0200

    Don't run apt-get update from recipes
    Running apt-get update inside recipes can cause problems if multiple
    Chef processes are running in parallel, which is the case for cloudinit.d
    launch plans with replicas. The apt database will be locked and the
    apt-get process will exit with status 100.
    
    Instead, use the apt recipe at the beginning of the basenode run_list.

--- cookbooks/virtualenv/recipes/default.rb
+++ cookbooks/virtualenv/recipes/default.rb
@@ -1,9 +1,5 @@
 case node[:platform]
   when "debian","ubuntu"
-    execute "update package index" do
-      command "apt-get update"
-      action :run
-    end
     package "python-virtualenv" do
       action :install
     end
--- cookbooks/zookeeper/recipes/default.rb
+++ cookbooks/zookeeper/recipes/default.rb
@@ -1,9 +1,5 @@
 case node[:platform]
   when "debian","ubuntu"
-    execute "update package index" do
-      command "apt-get update"
-      action :run
-    end
     package "zookeeperd" do
       action :install
     end
