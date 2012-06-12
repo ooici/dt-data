488d0990c726481f519fc5fb37c2cd20adad7540
Patrick Armstrong <patricka@uvic.ca>
2012-06-11 17:13:27 -0700

    Set more defaults with new epu cookbook

--- cookbooks/epu/attributes/default.rb
+++ cookbooks/epu/attributes/default.rb
@@ -1,2 +1,12 @@
 set_unless[:epu][:virtualenv][:python] = "python"
 set_unless[:epu][:virtualenv][:virtualenv] = "virtualenv"
+set_unless[:epu][:retrieve_config] = {}
+set_unless[:epu][:action] = {}
+set_unless[:epuagent][:virtualenv][:python] = "python"
+set_unless[:epuagent][:virtualenv][:virtualenv] = "virtualenv"
+set_unless[:epuagent][:retrieve_config] = {}
+set_unless[:epuagent][:action] = {}
+set_unless[:pyon][:virtualenv][:python] = "python"
+set_unless[:pyon][:virtualenv][:virtualenv] = "virtualenv"
+set_unless[:pyon][:retrieve_config] = {}
+set_unless[:pyon][:action] = {}
