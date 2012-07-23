1612b05dd25356cfd220aa9cb0cbf3c3f45fd372
David LaBissoniere <labisso@gmail.com>
2012-07-23 14:06:34 -0500

    Add centos support to zookeeper cookbook

--- cookbooks/zookeeper/recipes/default.rb
+++ cookbooks/zookeeper/recipes/default.rb
@@ -1,8 +1,28 @@
+service_name = nil
 case node[:platform]
   when "debian","ubuntu"
     package "zookeeperd" do
       action :install
     end
+
+    service_name = "zookeeper"
+
+  when "redhat","centos"
+    yum_key "RPM-GPG-KEY-cloudera" do
+      url "http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera"
+      action :add
+    end
+
+    yum_repository "cloudera-cdh4" do
+      description "Cloudera"
+      url "http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/4/"
+      key "RPM-GPG-KEY-cloudera"
+      action :add
+    end
+
+    package "zookeeper-server"
+
+    service_name = "zookeeper-server"
 end
 
 template "/etc/zookeeper/conf/zoo.cfg" do
@@ -19,7 +39,12 @@ template "#{node[:zookeeper][:dataDir]}/myid" do
     only_if { node[:zookeeper][:name].split("-")[1] }
 end
 
-service "zookeeper" do                                                          
+case node[:platform]
+  when "redhat","centos"
+    execute "/etc/init.d/zookeeper-server init"
+end
+
+service service_name do
   supports :status => true, :restart => true, :reload => true                   
   action [ :enable, :restart ]                                                    
 end    
