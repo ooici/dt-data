2fe7fc7400e4051eb01dd3720fd7504aaf3c44d9
David LaBissoniere <labisso@gmail.com>
2012-09-19 17:37:09 -0500

    Fix ddns recipe

--- cookbooks/ddns/recipes/default.rb
+++ cookbooks/ddns/recipes/default.rb
@@ -2,18 +2,20 @@ hostname = node[:ddns][:hostname]
 
 if hostname and !hostname.empty?
 
-  bash "update DDNS: #{hostname} -> #{node[:fqdn]}" do
-    code <<-EOH
+  commands <<-EOH
+  server #{node[:ddns][:server]}
+  update delete #{hostname} cname
+  update add #{hostname} #{node[:ddns][:ttl]} cname #{node[:fqdn]}
+  send
+  EOH
 
-    nsupdate << EOF
-    server #{node[:ddns][:server]}
-    update delete #{hostname} cname
-    update add #{hostname} #{node[:ddns][:ttl]} cname #{node[:fqdn]}
-    send
-    EOF
-    EOH
+  file "/tmp/ddns_nsupdate" do
+    content commands
+  end
+
+  execute "update DDNS: #{hostname} -> #{node[:fqdn]}" do
+    command "nsupdate /tmp/ddns_nsupdate"
   end
 
   # do nothing if hostname is not provided or is empty.
-
 end
