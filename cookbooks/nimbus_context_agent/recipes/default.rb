74c447ba6dbad9e627e52438179f3a21065e3dbe
David LaBissoniere <labisso@gmail.com>
2012-10-19 12:16:12 -0500

    Remove old workarounds in ctx agent cookbook

--- cookbooks/nimbus_context_agent/recipes/default.rb
+++ cookbooks/nimbus_context_agent/recipes/default.rb
@@ -2,20 +2,10 @@
 
 bash "install-nimbus-context-agent" do
   code <<-EOH
-    if [ -d /etc/apt ]; then
-      wget http://ooi.s3.amazonaws.com/nimbus-ctx-agent-2.3.0-ALT.tar.gz
-      tar zxf nimbus-ctx-agent-2.3.0-ALT.tar.gz
-      mv nimbus-ctx-agent-2.3.0-ALT/ /opt/nimbus/
-    else
-      if [ ! -d /opt/nimbus ]; then
-        wget http://www.nimbusproject.org/downloads/nimbus-ctx-agent-2.3.0.tar.gz
-        tar zxf nimbus-ctx-agent-2.3.0.tar.gz
-        mv nimbus-ctx-agent-2.3.0/ /opt/nimbus/
-      fi
-      if [ -d /opt/nimbus/nimbus-ctx-agent-2.3.0 ]; then
-        mv /opt/nimbus/ /opt/oldnimbus
-        mv /opt/oldnimbus/nimbus-ctx-agent-2.3.0 /opt/nimbus
-      fi
+    if [ ! -d /opt/nimbus ]; then
+      wget http://www.nimbusproject.org/downloads/nimbus-ctx-agent-2.3.0.tar.gz
+      tar zxf nimbus-ctx-agent-2.3.0.tar.gz
+      mv nimbus-ctx-agent-2.3.0/ /opt/nimbus/
     fi
   EOH
 end
