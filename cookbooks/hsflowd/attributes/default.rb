c5fd49f1234da3864e20ee325f88a6938a34bdd4
Patrick Armstrong <patricka@uvic.ca>
2012-10-23 12:07:09 -0700

    Update hsflow to 1.22.2

--- cookbooks/hsflowd/attributes/default.rb
+++ cookbooks/hsflowd/attributes/default.rb
@@ -3,9 +3,9 @@ default[:hsflowd][:collector_ip] = "127.0.0.1"
 default[:hsflowd][:collector_port] = 6343
 default[:hsflowd][:json_port] = 36343
 default[:hsflowd][:polling_interval] = 30
-default[:hsflowd][:src_version] = "1.22.1"
-default[:hsflowd][:src_name] = "hsflowd-1.22.1.tar.gz"
-default[:hsflowd][:src_checksum] = "2dfc17288918b61b3b8c5d34cdac9dcf"
+default[:hsflowd][:src_version] = "1.22.2"
+default[:hsflowd][:src_name] = "hsflowd-1.22.2.tar.gz"
+default[:hsflowd][:src_checksum] = "823d33a8405f38a9c37bdee994fb2199"
 default[:hsflowd][:src_tarball_url] = "http://downloads.sourceforge.net/project/host-sflow/REL-1_22/hsflowd-1.22.1.tar.gz"
 default[:hsflowd][:patches] = [] # A list of URLs or patches to apply
 
