f804b1ee5784b82f99ed0ecfecd3b76d39c70ffb
Patrick Armstrong <patricka@uvic.ca>
2012-10-23 12:18:05 -0700

    Fix typo in hsflow url

--- cookbooks/hsflowd/attributes/default.rb
+++ cookbooks/hsflowd/attributes/default.rb
@@ -6,7 +6,7 @@ default[:hsflowd][:polling_interval] = 30
 default[:hsflowd][:src_version] = "1.22.2"
 default[:hsflowd][:src_name] = "hsflowd-1.22.2.tar.gz"
 default[:hsflowd][:src_checksum] = "823d33a8405f38a9c37bdee994fb2199"
-default[:hsflowd][:src_tarball_url] = "http://downloads.sourceforge.net/project/host-sflow/REL-1_22/hsflowd-1.22.1.tar.gz"
+default[:hsflowd][:src_tarball_url] = "http://downloads.sourceforge.net/project/host-sflow/REL-1_22/hsflowd-1.22.2.tar.gz"
 default[:hsflowd][:patches] = [] # A list of URLs or patches to apply
 
 
