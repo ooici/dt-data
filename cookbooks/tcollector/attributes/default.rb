f5b9ddee37ca4d3c67e61c833a3dadbee1db1aca
Patrick Armstrong <patricka@uvic.ca>
2012-11-06 16:50:45 -0800

    Add tcollector cookbook

---
+++ cookbooks/tcollector/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:tcollector][:git_repository] = "https://github.com/OpenTSDB/tcollector.git"
+default[:tcollector][:logfile] = "/var/log/tcollector.log"
+default[:tcollector][:tsd_host] = "localhost"
+
+
