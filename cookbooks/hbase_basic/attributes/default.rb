ac0a669208f4e0cb5cdb1a676f23f9886ab030b6
Pierre Riteau <priteau@ci.uchicago.edu>
2012-12-14 16:56:55 +0000

    Mirror hbase tarball for faster deployment

--- cookbooks/hbase_basic/attributes/default.rb
+++ cookbooks/hbase_basic/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:hbase][:source] = "http://apache.sunsite.ualberta.ca/hbase/hbase-0.94.2/hbase-0.94.2.tar.gz"
+default[:hbase][:source] = "http://www.nimbusproject.org/downloads/hbase-0.94.2.tar.gz"
 default[:hbase][:root_dir] = "/tmp/hbase"
 default[:hbase][:zookeeper][:data_dir] = "/tmp/hbase-zk"
