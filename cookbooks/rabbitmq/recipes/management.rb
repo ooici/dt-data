d0b516a94e782b1144e51f83e0010fd27bb19e08
Patrick Armstrong <patricka@uvic.ca>
2013-01-30 14:10:22 -0800

    Restart rabbit after enabling console

--- cookbooks/rabbitmq/recipes/management.rb
+++ cookbooks/rabbitmq/recipes/management.rb
@@ -2,3 +2,8 @@ execute "Enable RabbitMQ Management Plugin" do
   command "rabbitmq-plugins enable rabbitmq_management"
 end
 
+# Restart rabbit for older versions of rabbit
+service "rabbitmq-server" do
+  supports :restart => true
+  action :restart
+end
