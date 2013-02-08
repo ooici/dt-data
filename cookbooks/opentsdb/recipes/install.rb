a27dbe7cfe36fad8f15acbabfa9412b42c9f6cda
Pierre Riteau <priteau@ci.uchicago.edu>
2013-02-08 10:37:38 +0000

    This workaround is not longer needed.

--- cookbooks/opentsdb/recipes/install.rb
+++ cookbooks/opentsdb/recipes/install.rb
@@ -1,3 +1,4 @@
+
 # Git resource seems broken?
 script "Extract OpenTSDB" do
   interpreter "bash"
@@ -5,11 +6,6 @@ script "Extract OpenTSDB" do
   git clone #{node[:opentsdb][:git_url]} opentsdb
   EOH
   cwd "/opt"
-end
-
-execute "Patch OpenTSDB to fetch junit from our server" do
-  command "sed -i -e 's%http://cloud.github.com/downloads/KentBeck/junit%http://www.nimbusproject.org/downloads%' third_party/junit/include.mk"
-  cwd "/opt/opentsdb"
 end
 
 package "gnuplot" do
