da64b315dd12d8b607d555be0ed03be062fce0e0
Patrick Armstrong <patricka@uvic.ca>
2013-02-12 14:11:19 -0800

    Add Exceptional API key setting to phantomweb

--- cookbooks/phantomweb/attributes/default.rb
+++ cookbooks/phantomweb/attributes/default.rb
@@ -1,3 +1,4 @@
 set_unless[:virtualenv][:python] = "python"
 set_unless[:virtualenv][:virtualenv] = "virtualenv"
 set_unless[:phantomweb][:google_analytics_code] = ""
+set_unless[:phantomweb][:exceptional_api_key] = ""
