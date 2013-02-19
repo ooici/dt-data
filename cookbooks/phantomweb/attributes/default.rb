3e777512836cf42cc49281ce72646d0b2a72d719
Patrick Armstrong <patricka@uvic.ca>
2013-02-18 16:29:40 -0800

    Add missing phantomweb install_config stub

--- cookbooks/phantomweb/attributes/default.rb
+++ cookbooks/phantomweb/attributes/default.rb
@@ -2,3 +2,4 @@ set_unless[:virtualenv][:python] = "python"
 set_unless[:virtualenv][:virtualenv] = "virtualenv"
 set_unless[:phantomweb][:google_analytics_code] = ""
 set_unless[:phantomweb][:exceptional_api_key] = ""
+set_unless[:phantomweb][:install_config] = {}
